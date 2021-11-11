5c4aedb8aa8646d985c6e2d00a8568de722e3ba8
HannesPreishuber <hannesp@ppedv.de>
2021-11-11 10:14:30 +0100

    Trennen UI Logik

---
+++ BlazorMUCsrv/Pages/modul1/Hannes.razor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMUCsrv.Pages.modul1
+{
+    public partial class Hannes
+    {
+
+    }
+}
