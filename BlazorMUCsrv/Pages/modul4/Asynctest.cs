a6da079b2a547c07dbeb442118cdf692db99286b
HannesPreishuber <hannesp@ppedv.de>
2021-11-12 11:53:43 +0100

    Async To List per using Microsoft.EntityFrameworkCore

---
+++ BlazorMUCsrv/Pages/modul4/Asynctest.cs
@@ -0,0 +1,18 @@
+﻿using BlazorMUCsrv.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMUCsrv.Pages.modul4
+{
+    public class Asynctest
+    {
+        public Asynctest()
+        {
+            var x = new northwindContext();
+            var y = x.Customers.ToListAsync(); //using Microsoft.EntityFrameworkCore;
+        }
+    }
+}
