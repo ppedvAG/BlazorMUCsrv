ecd7ba0d5caa6b1966901ba594f28a60177a3eaf
HannesPreishuber <hannesp@ppedv.de>
2021-11-12 11:02:05 +0100

    HTML INput Varianten

---
+++ BlazorMUCsrv/Pages/modul5/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMUCsrv.Pages.modul5
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage ="da muss was rein")]
+        [StringLength(45,ErrorMessage ="max 45 Zeichen")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "da muss was rein")]
+        public DateTime GebDat { get; set; }
+        [Required(ErrorMessage = "da muss was rein")]
+        [Range(140,200, ErrorMessage ="keine Achterbahn für dich")]
+        public int Gross { get; set; }
+    }
+}
---
+++ BlazorMUCsrv/Pages/modul5/html.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorMUCsrv.Pages.modul5
+{
+    public class htmlModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
